a0b5fc2b1dc511b9e6845e2306bf24766991fdbb
Kotagiri <Hareesh.Kotagiri@pepsico.com>
2022-12-20 16:16:57 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -33,6 +33,9 @@ const App: () => React$Node = () => {
               <Text style={styles.sectionTitle}>Hello PepsiCo!</Text>
             </View>
             <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Welcome DPS world</Text>
+            </View>
+            <View style={styles.sectionContainer}>
               <Text style={styles.sectionDescription}>
                 This application will work in mobile and web at once
               </Text>
